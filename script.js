3bfc5a75497ed31755adaf9bf5a8b7db4366c06e
Farid Hussain <faridhussain0011@gmail.com>
2025-11-29 22:55:03 +0530

    done till interval

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 const board = document.querySelector(".board");
-const blockHeight = 80;
-const blockWeight = 80;
+const blockHeight = 50;
+const blockWeight = 50;
 
 const cols = Math.floor(board.clientWidth / blockWeight);
 const rows = Math.floor(board.clientHeight / blockHeight);
 
-const blocks = [];
+let interval = null;
+let food = {
+  x: Math.floor(Math.random() * rows),
+  y: Math.floor(Math.random() * cols),
+};
 
-// for (let i = 0; i < rows * cols; i++) {
-//   const block = document.createElement("div");
-//   block.classList.add("block");
-//   board.appendChild(block);
-// }
+const blocks = [];
+const snake = [
+  {
+    x: 1,
+    y: 3,
+  },
+];
+let direction = "down";
 
 for (let row = 0; row < rows; row++) {
   for (let col = 0; col < cols; col++) {
@@ -19,5 +26,52 @@ for (let row = 0; row < rows; row++) {
     block.classList.add("block");
     board.appendChild(block);
     block.innerText = `${row}-${col}`;
+    blocks[`${row}-${col}`] = block;
   }
 }
+
+function render() {
+  let head = null;
+
+  blocks[`${food.x}-${food.y}`].classList.add("food");
+
+  if (direction === "left") {
+    head = { x: snake[0].x, y: snake[0].y - 1 };
+  } else if (direction === "right") {
+    head = { x: snake[0].x, y: snake[0].y + 1 };
+  } else if (direction === "down") {
+    head = { x: snake[0].x + 1, y: snake[0].y };
+  } else if (direction === "up") {
+    head = { x: snake[0].x - 1, y: snake[0].y };
+  }
+  if (head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols) {
+    alert("Game Over");
+    clearInterval(interval);
+  }
+
+  snake.forEach((segment) => {
+    blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
+  });
+
+  snake.unshift(head);
+  snake.pop();
+  snake.forEach((segment) => {
+    blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+  });
+}
+
+interval = setInterval(() => {
+  render();
+}, 300);
+
+addEventListener("keydown", (evt) => {
+  if (evt.key === "ArrowUp") {
+    direction = "up";
+  } else if (evt.key === "ArrowRight") {
+    direction = "right";
+  } else if (evt.key === "ArrowDown") {
+    direction = "down";
+  } else if (evt.key === "ArrowLeft") {
+    direction = "left";
+  }
+});
